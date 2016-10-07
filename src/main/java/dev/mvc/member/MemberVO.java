8774a4759869142cf82d95242123cef005eb2bc3
soldesk <soldesk@soldeskPC-1-25>
2016-10-07 17:22:59 +0900

    vo 고고

--- src/main/java/dev/mvc/member/MemberVO.java
+++ src/main/java/dev/mvc/member/MemberVO.java
@@ -2,4 +2,19 @@ package dev.mvc.member;
 
 public class MemberVO {
 
+  private int me_no;
+  private String me_name;
+  public int getMe_no() {
+    return me_no;
+  }
+  public void setMe_no(int me_no) {
+    this.me_no = me_no;
+  }
+  public String getMe_name() {
+    return me_name;
+  }
+  public void setMe_name(String me_name) {
+    this.me_name = me_name;
+  }
+
 }
