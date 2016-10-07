fb439e29cc613e367266548da0f38ad55aa31bbb
test <seehanpark@cyworld.com>
2016-10-07 17:45:03 +0900

    DTD

--- src/main/java/dev/mvc/enterprise_info/EnterpriseInformationVO.java
+++ src/main/java/dev/mvc/enterprise_info/EnterpriseInformationVO.java
@@ -3,7 +3,7 @@ package dev.mvc.enterprise_info;
 public class EnterpriseInformationVO { //
   
   public static void main(String [] args) {
-    System.out.println("EnterpriseInformat1ionVO");
+    System.out.println("EnterpriseInformationVO1");
   }
 
 }
