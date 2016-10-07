98e53b572cbec7329bc85363758e0076c4d0a5bd
test <seehanpark@cyworld.com>
2016-10-07 17:20:54 +0900

    EnterpriseInformationVO

--- src/main/java/dev/mvc/enterprise_info/EnterpriseInformationVO.java
+++ src/main/java/dev/mvc/enterprise_info/EnterpriseInformationVO.java
@@ -1,5 +1,9 @@
 package dev.mvc.enterprise_info;
 
-public class EnterpriseInformationVO { //  //
+public class EnterpriseInformationVO { //
+  
+  public static void main(String [] args) {
+    System.out.println("EnterpriseInformationVO");
+  }
 
 }
