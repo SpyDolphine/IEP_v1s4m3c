2c1d288f009806c4c4dcf6d35c4e31303636f405
soldesk <soldesk@soldeskPC-1-25>
2016-10-07 17:42:51 +0900

    회원 vo 완료

--- src/main/java/dev/mvc/member/MemberVO.java
+++ src/main/java/dev/mvc/member/MemberVO.java
@@ -3,7 +3,71 @@ package dev.mvc.member;
 public class MemberVO {
 
   private int me_no;
+  private String me_id;
+  private String me_pw;
+  private String me_nick;
   private String me_name;
+  private String me_grade;
+  private String me_zipcode;
+  private String me_addr1;
+  private String me_addr2;
+  private String me_tel;
+  private String me_date;
+  
+  public String getMe_id() {
+    return me_id;
+  }
+  public void setMe_id(String me_id) {
+    this.me_id = me_id;
+  }
+  public String getMe_pw() {
+    return me_pw;
+  }
+  public void setMe_pw(String me_pw) {
+    this.me_pw = me_pw;
+  }
+  public String getMe_nick() {
+    return me_nick;
+  }
+  public void setMe_nick(String me_nick) {
+    this.me_nick = me_nick;
+  }
+  public String getMe_grade() {
+    return me_grade;
+  }
+  public void setMe_grade(String me_grade) {
+    this.me_grade = me_grade;
+  }
+  public String getMe_zipcode() {
+    return me_zipcode;
+  }
+  public void setMe_zipcode(String me_zipcode) {
+    this.me_zipcode = me_zipcode;
+  }
+  public String getMe_addr1() {
+    return me_addr1;
+  }
+  public void setMe_addr1(String me_addr1) {
+    this.me_addr1 = me_addr1;
+  }
+  public String getMe_addr2() {
+    return me_addr2;
+  }
+  public void setMe_addr2(String me_addr2) {
+    this.me_addr2 = me_addr2;
+  }
+  public String getMe_tel() {
+    return me_tel;
+  }
+  public void setMe_tel(String me_tel) {
+    this.me_tel = me_tel;
+  }
+  public String getMe_date() {
+    return me_date;
+  }
+  public void setMe_date(String me_date) {
+    this.me_date = me_date;
+  }
   public int getMe_no() {
     return me_no;
   }
